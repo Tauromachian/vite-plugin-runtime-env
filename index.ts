ff90e99e3b7dcd9fe3d1eb589ce5d7d07031547d
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-06-21 15:12:39 -0400

    First commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import type { Plugin } from "vite";
+
+export default function runtimeEnv(): Plugin {
+  return {
+    name: "runtime-env",
+    transform: (code: string) => {
+      const newCode = code.replace(/import\.meta\.env/, "window.env");
+
+      return newCode;
+    },
+    transformIndexHtml: () => {
+      const script =
+        'import envVars from \'"env.js"; window.env = { ...window.env, ...envVars }';
+
+      return [
+        {
+          tag: "script",
+          attrs: {
+            type: "module",
+          },
+          children: script,
+          injectTo: "head",
+        },
+      ];
+    },
+  };
+}

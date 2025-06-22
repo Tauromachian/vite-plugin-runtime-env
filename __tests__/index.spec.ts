61bc7e11209258e695d360ae07b79c91a6c965aa
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-06-22 13:12:21 -0400

    Unit test script injection

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -9,6 +9,7 @@ import runtimeEnv from "..";
 describe("vite-plugin-runtime-env", () => {
   const testDir = path.resolve(__dirname, "fixtures");
   const testFile = path.resolve(testDir, "entry.js");
+  const testHtml = path.resolve(testDir, "index.html");
 
   describe("transform", () => {
     it("replaces import.meta.env with window.env", async () => {
@@ -51,6 +52,78 @@ describe("vite-plugin-runtime-env", () => {
       expect(code).toContain("window.lol.SOME_OTHER_ENV");
 
       expect(code).to.not.contain("import.meta.env.SOME_OTHER_ENV");
+    });
+
+    it("Injects the env.js loading script", async () => {
+      const result = await build({
+        root: testDir,
+        logLevel: "silent",
+        plugins: [runtimeEnv()],
+        build: {
+          write: false,
+          rollupOptions: {
+            input: testHtml,
+          },
+        },
+      });
+
+      expect(result.output[0].source).toContain(
+        'import envVars from \'"env.js"; window.env = { ...window.env, ...envVars }',
+      );
+    });
+
+    it("Injects the script with name specified in the config", async () => {
+      const result = await build({
+        root: testDir,
+        logLevel: "silent",
+        plugins: [runtimeEnv({ jsFileName: "config" })],
+        build: {
+          write: false,
+          rollupOptions: {
+            input: testHtml,
+          },
+        },
+      });
+
+      expect(result.output[0].source).toContain(
+        'import envVars from \'"config.js"; window.env = { ...window.env, ...envVars }',
+      );
+    });
+
+    it("Sets the global variables specified in the config", async () => {
+      const result = await build({
+        root: testDir,
+        logLevel: "silent",
+        plugins: [runtimeEnv({ globalObjectName: "i_am_batman" })],
+        build: {
+          write: false,
+          rollupOptions: {
+            input: testHtml,
+          },
+        },
+      });
+
+      expect(result.output[0].source).toContain(
+        'import envVars from \'"env.js"; window.i_am_batman = { ...window.i_am_batman, ...envVars }',
+      );
+    });
+
+    it("Won't inject script if specified in the configuration file", async () => {
+      const result = await build({
+        root: testDir,
+        logLevel: "silent",
+        plugins: [runtimeEnv({ injectScript: false })],
+        build: {
+          write: false,
+          rollupOptions: {
+            input: testHtml,
+          },
+        },
+      });
+
+      expect(result.output[0].source).to.not.contain(
+        'import envVars from \'"env.js"; window.env = { ...window.env, ...envVars }',
+      );
     });
   });
 });
